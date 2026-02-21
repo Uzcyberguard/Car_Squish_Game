67afddceb0d66e95d74d26d9297c2e90b620a2dd
Uzcyberguard <hasannormamatov696@gmail.com>
2026-02-21 15:42:23 +0500

    changed

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from PIL import Image
 from turtle import Turtle, Screen
 import os
 
-screen = Screen()
-screen.setup(width=800, height=600)
+from PIL.ImageOps import expand
 
+screen = Screen()
+screen.setup(width=600, height=700)
+screen.bgcolor("gray25")
 gif_files = [f"car{i}.gif" for i in range(1, 16)]
 for pic in gif_files:
+
     screen.addshape(pic)
 
 
@@ -19,6 +22,8 @@ for pic in gif_files:
 tim = Turtle()
 tim.shape(gif_files[8])
 tim.penup()
-
-
+tim.setheading(90)
+tim.goto(0,-300)
+tim2 = Turtle()
+tim2.shape(gif_files[10])
 screen.exitonclick()
